75fa1d1003748b230d646695f36afb68b3121650
Florian Kapfenberger <florian@kapfenberger.me>
2017-11-15 13:57:08 +0100

    added some gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,17 @@ module.exports = {
     title: `Florian Kapfenberger`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-51334401-3",
+        // Setting this parameter is optional
+        anonymize: true
+      }
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-preact`
+    `gatsby-plugin-preact`,
+    `gatsby-plugin-netlify`
   ]
 };
