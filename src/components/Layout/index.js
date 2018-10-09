9fcae94a7d4220c0c95c5a87acdd38ce545e4f58
Florian Kapfenberger <florian@kapfenberger.me>
2018-10-09 13:38:18 +0200

    removed StaticQuery

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,7 +8,6 @@ import {
   faPaperPlane,
   faBars,
 } from '@fortawesome/free-solid-svg-icons'
-import { StaticQuery, graphql } from 'gatsby'
 
 import 'normalize.css'
 import './index.css'
@@ -20,40 +19,25 @@ console.log(
   'If you want to see the code, you can find it on GitHub: https://github.com/phiilu/site :D'
 )
 
-export const metaDataQuery = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
-
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={metaDataQuery}
-    render={data => (
-      <div>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: 'description',
-              content:
-                'This is my personal website. I am a Frontend Developer and this site allows me to show of some of my work.',
-            },
-            {
-              name: 'keywords',
-              content:
-                'web developer, frontend developer, webentwickler, reactjs developer, reactjs webentwickler, react, reactjs, programmierer, persönliche website, portfolio',
-            },
-          ]}
-        />
-        {children}
-      </div>
-    )}
-  />
+  <div>
+    <Helmet
+      title="Florian Kapfenberger | Frontend Developer"
+      meta={[
+        {
+          name: 'description',
+          content:
+            'This is my personal website. I am a Frontend Developer and this site allows me to show of some of my work.',
+        },
+        {
+          name: 'keywords',
+          content:
+            'web developer, frontend developer, webentwickler, reactjs developer, reactjs webentwickler, react, reactjs, programmierer, persönliche website, portfolio',
+        },
+      ]}
+    />
+    {children}
+  </div>
 )
 
 export default Layout
