76c2053009f52d1049c89cc5b0846951ca9ee014
Florian Kapfenberger <florian@kapfenberger.me>
2018-10-30 19:58:24 +0100

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,39 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+
+        // WebApp Manifest Configuration
+        appName: 'Florian Kapfenberger', // Inferred with your package.json
+        appDescription: null,
+        developerName: 'Florian Kapfenberger',
+        developerURL: 'https://github.com/phiilu',
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#DE092D',
+        theme_color: '#DE092D',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-netlify',
